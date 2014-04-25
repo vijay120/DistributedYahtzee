e0e87e1e2fb986858ed0a2775b7fbcf111d8118c
Tum Chaturapruek <tummykung@gmail.com>
2014-04-25 01:17:52 -0700

    Add messages for players.

--- distributed_yahtzee.erl
+++ distributed_yahtzee.erl
@@ -60,33 +60,150 @@ listen() ->
         %% =============================================================
         %%                System <-> External World Functions
         %% =============================================================
-        {request_tournament, Pid, {NumPlayer, GamePerMatch}} ->
+        % request-tournament - data is a tuple
+        %   { num-players, games-per-match };
+        %
+        %   > num-players is the number of players that should
+        %       participate in the tournament.
+        %
+        %   > games-per-match is the (maximum) number of games to play for
+        %       match e.g., for a tournament with best-of-5 matches,
+        %       games-per-match would be 5). A request_tournament message
+        %       with an invalid games-per-match value (such as an even
+        %       number, or a negative number) is ignored.
+        {request_tournament, Pid, {NumPlayers, GamePerMatch}} ->
+            println("~s > request_tournament message received from ~p with " ++
+                "num-players = ~p, games-per-match = ~p.",
+                [node(), Pid, NumPlayers, GamePerMatch]),
             Tid = ?TEMP,
             Players = [?TEMP, ?TEMP],
             OptionalData = [],
             Pid ! {tournament_started, Pid, {Tid, Players, OptionalData}};
             
+        % tournament-info - data is a tournament ID 
         {tournament_info, Pid, {TournamentId}} ->
+            println("~s > tournament_info message received from ~p with " ++
+                "tournament ID = ~p.", [node(), Pid, TournamentId]),
             Status = ?TEMP,
             Winner = ?TEMP,
             OptionalData = [],
-            Pid ! {tournament_status, Pid, {TournamentId, Status, Winner, OptionalData}};
+            Pid ! {tournament_status, Pid,
+                {TournamentId, Status, Winner, OptionalData}};
 
+        % user-info - data is a username
         {user_info, Pid, {Username}} ->
-            Username = ?TEMP,
+            println("~s > user_info message received from ~p with " ++
+                "username = ~p.", [node(), Pid, Username]),
             MatchWins = [?TEMP, ?TEMP],
             MatchLosses = [?TEMP, ?TEMP],
             TournamentsPlayed = [?TEMP, ?TEMP],
             TournamentWins = [?TEMP, ?TEMP],
-            Pid ! {user_status, Pid, {Username, MatchWins, MatchLosses, TournamentsPlayed, TournamentWins}};
+            Pid ! {user_status, Pid,
+                {Username, MatchWins, MatchLosses,
+                TournamentsPlayed, TournamentWins}};
         %% ==============================================================
         %%                   System <-> Player Functions
         %% ==============================================================
+
+        % The following message types can be sent from a player to the
+        % rest of the system:
+
+        % login - data is a tuple
+        %     { username, password }.
+        %
+        %     > username and password are strings with which the player
+        %         wants to log in for tournament play. If a username was
+        %         not previously registered with the system, it is registered
+        %         when the first login is received for that username.
+        %         There is no mechanism for changing passwords.
+
+        {login, Pid, {Username, Password}} ->
+            println("~s > login message received from ~p with " ++
+                "username = ~p, password = ~p.",
+                [node(), Pid, Username, Password]);
+
+        % logout - data is a
+        %    { login-ticket };
+        %
+        %     which was acquired in the most recent
+        %       logged-in message.
+        %     This logs the player out (making them ineligible for
+        %       playing in tournaments until they log in again).
+
+        {logout, Pid, {LoginTicket}} ->
+            println("~s > logout message received from ~p with " ++
+                "login-ticket = ~p.", [node(), Pid, LoginTicket]);
+
+        % accept_tournament - data is a tuple
+        %     { tid, login-ticket };
+        %
+        %     > tid is the tournament ID that the player received in
+        %        a start-tournament message, and
+        %
+        %     > login-ticket is the login ticket the player received
+        %        on its current login. This message indicates that the
+        %        player is willing to play in the specified tournament.
+        %
+        %     > The pid-for-reply in this message determines where
+        %         play-request messages for this player in the specified
+        %         tournament are sent.
+        {accept_tournament, Pid, {Tid, LoginTicket}} ->
+            println("~s > accept_tournament message received from ~p with " ++
+                "tid = ~p, login-ticket = ~p.", [node(), Pid, Tid, LoginTicket]);
+
+        % reject_tournament - data is a tuple
+        %     { tid, login-ticket };
+        %
+        %     > tid is the tournament ID that the player received in a
+        %       start-tournament message, and
+        %
+        %     > login-ticket is the login ticket the player received on
+        %         its current login. This message indicates that the player
+        %         is not willing to play in the specified tournament.
+        %         These messages are optional - if a player does not send a reply
+        %         (or sends a malformed reply, such as one with a bad login-ticket)
+        %         to a start_tournament message, it is assumed to have rejected
+        %         the tournament; thus, tournament managers must use timeouts
+        %         when setting up tournaments.
+        {reject_tournament, Pid, {Tid, LoginTicket}} ->
+            println("~s > reject_tournament message received from ~p with " ++
+                "tid = ~p, login-ticket = ~p.", [node(), Pid, Tid, LoginTicket]);
+
+        % play_action - data is a tuple
+        %     { ref, tid, gid, roll-number, dice-to-keep, scorecard-line }.
+        %
+        %     This message is sent in response to a play_request message:
+        %     > ref, tid, gid and roll-number are the same values received
+        %        in the play_request message;
+        %
+        %     > dice-to-keep is a list of 5 booleans (true or false atoms)
+        %         representing whether to keep each of the dice in the dice
+        %         list from the play_request message; and
+        %
+        %     > scorecard-line is an integer representing a line on the
+        %        scorecard in which to score the dice.
+        %
+        %     > 1-13 represent lines on the scorecard in the order presented
+        %        in the homework assignment, while 0 represents no scoring for
+        %        this action. Sending a non-zero value for scorecard-line ends
+        %        the round for the player and scores that line in the scorecard;
+        %        it is a violation of the protocol for the player to send 0 for
+        %        scorecard-line on roll-number 3.
+        {play_action, Pid, {Ref, Tid, Gid, RollNumber, DiceToKeep, ScorecardLine}} ->
+            println("~s > play_action message received from ~p with " ++
+                "ref = ~p, tid = ~p, gid = ~p",
+                [node(), Pid, Tid, Gid]),
+            println("~s > roll-number = ~p, dice-to-keep = ~p, scorecard-line = ~p",
+                [node(), RollNumber, DiceToKeep, ScorecardLine]);
+
+
         %% ==============================================================
         %%                             Else
         %% ==============================================================
-        {_, Pid, _Data} ->
-            Pid ! {error, "Unexpected message."}
+        {MessageType, Pid, Data} ->
+            println("~s > Malformed ~p message from ~p with data = ~p",
+                [node(), MessageType, Pid, Data]),
+            Pid ! {error, "Malformed message."}
     end,
     listen().
 
--- external_controller.erl
+++ external_controller.erl
@@ -10,7 +10,7 @@
 % {ash:1} erl -noshell -run external_controller main $node_name $action
 
 % action = 
-%   request_tournament NumPlayer GamePerMatch
+%   request_tournament NumPlayers GamePerMatch
 %   tournament_info TournamentId
 %   user_info Username
 
@@ -22,33 +22,8 @@
 
 -import(distributed_yahtzee, [println/1, println/2]).
 
-% The following message types can be sent from the rest of the system to the outside world:
-
-%     tournament-started - data is a tuple { tid, players, optional-data } where
-%         > tid is the tournament ID,
-%         > players is a list of the usernames assigned to play in the tournament,
-%         and optional-data is optional implementation-dependent data about the tournament
-%           (such as a representation of the bracket).
-%     (Sent in response to a start-tournament.)
-%
-%     tournament-status - data is a tuple
-%         { tid, status, winner, optional-data } where
-%         > tid is the tournament ID,
-%         > status is an atom indicating the tournament status (in-progress, complete),
-%         winner is
-%             the username of the winner, if a winner has been determined, or
-%             undefined if not, and optional-data is optional implementation-dependent
-%               data about the tournament (such as a representation of the bracket).
-%     (Sent in response to a tournament-info.)
-%     A tournament-info requesting information about an invalid tournament ID is ignored.
-%
-%     user-status - data is a tuple
-%       { username, match-wins, match-losses, tournaments-played, tournament-wins } where
-%       > username is a username and the other components are all integers and mostly self-explanatory;
-%       > tournaments-played should include tournaments in progress that the user has agreed to participate in,
-%         since once a user is in a tournament a result will be recorded for them whether or not their
-%         player crashes. Sent in response to a user-info. A user-info requesting information about an
-%         invalid username is ignored.
+% The following message types can be sent from the rest of the system
+% to the outside world:
 
 % loop() ->
 %   loop_once(),
@@ -56,17 +31,57 @@
 
 loop_once() ->
   receive
+    % tournament-started - data is a tuple
+    %     { tid, players, optional-data } where
+    %
+    %     > tid is the tournament ID,
+    %
+    %     > players is a list of the usernames assigned to play in the
+    %        tournament, and optional-data is optional implementation-dependent
+    %        data about the tournament (such as a representation of the bracket).
+    %        (Sent in response to a start-tournament.)
     {tournament_started, Pid, {Tid, Players, OptionalData}} ->
-      println("~s: > The list of players is ~p", [?GlobalName, Players]);
+      println("~s: > The list of players is ~p",
+        [?GlobalName, Players]);
 
+    % tournament-status - data is a tuple
+    %     { tid, status, winner, optional-data } where
+    %
+    %     > tid is the tournament ID,
+    %
+    %     > status is an atom indicating the tournament status (in-progress, complete),
+    %     winner is
+    %         the username of the winner, if a winner has been determined, or
+    %         undefined if not, and optional-data is optional implementation-dependent
+    %           data about the tournament (such as a representation of the bracket).
+    % (Sent in response to a tournament-info.)
+    % A tournament-info requesting information about an invalid tournament ID is ignored.
     {tournament_status, Pid, {Tid, Status, Winner, OptionalData}} ->
-      println("~s: > The tournament status is ~p. The winner is ~p", [?GlobalName, Status, Winner]);
+      println("~s: > The tournament status is ~p. The winner is ~p",
+        [?GlobalName, Status, Winner]);
 
-    {user_status, Pid, {Username, MatchWins, MatchLosses, TournamentsPlayed, TournamentWins}} ->
-      println("~s: > The match wins is ~p", [?GlobalName, MatchWins]),
-      println("~s: > The match losses is ~p", [?GlobalName, MatchLosses]),
-      println("~s: > The tournaments played are ~p", [?GlobalName, TournamentsPlayed]),
-      println("~s: > The tournaments wins is ~p", [?GlobalName, TournamentWins]);
+    % user-status - data is a tuple
+    %   { username, match-wins, match-losses, tournaments-played, tournament-wins } where
+    %
+    %   > username is a username and the other components are all integers
+    %     and mostly self-explanatory;
+    %
+    %   > tournaments-played should include tournaments in progress that
+    %     the user has agreed to participate in, since once a user is in a
+    %     tournament a result will be recorded for them whether or not their
+    %     player crashes. Sent in response to a user-info. A user-info requesting
+    %     information about an invalid username is ignored.
+    {user_status, Pid, {Username, MatchWins, MatchLosses,
+      TournamentsPlayed, TournamentWins}} ->
+      println("~s: > The match wins is ~p",
+        [?GlobalName, MatchWins]),
+      println("~s: > The match losses is ~p",
+        [?GlobalName, MatchLosses]),
+      println("~s: > The tournaments played are ~p",
+        [?GlobalName, TournamentsPlayed]),
+      println("~s: > The tournaments wins is ~p",
+        [?GlobalName, TournamentWins]);
+
     {error, Error} ->
       println("~s > Error: ~p", [?GlobalName, Error]);
     _ ->
@@ -81,7 +96,7 @@ main(Params) ->
   TheRest = tl(tl(Params)),
   net_kernel:start([external_controller, shortnames]),
 
-  % ======== START boilerplate code to connect =========
+  % ======== START boilerplate code to connect for global registry =========
   % case net_kernel:connect(list_to_atom(SystemManagerNode)) of
   %   true ->
   %     println("Connected to ~p successfully.", [SystemManagerNode]);
@@ -89,50 +104,64 @@ main(Params) ->
   %     println("Error: Cannot connect to ~p! Please try again", [SystemManagerNode]),
   %     halt()
   % end,
-  % ======== END boilerplate code to connect =========
+  % ======== END boilerplate code to connect for global registry =========
 
   % The following message types can be sent from the outside world to
   % the rest of the system:
 
-  %     request-tournament - data is a tuple { num-players, games-per-match };
-  %         num-players is the number of players that should participate in the tournament.
-  %         games-per-match is the (maximum) number of games to play for match 
-  %           e.g., for a tournament with best-of-5 matches, games-per-match would be 5). 
+  %     request-tournament - data is a tuple
+  %       { num-players, games-per-match };
+  %
+  %       > num-players is the number of players that should
+  %         participate in the tournament.
+  %
+  %       > games-per-match is the (maximum) number of games
+  %         to play for match  e.g., for a tournament with
+  %         best-of-5 matches, games-per-match would be 5). 
+  %
   %     tournament-info - data is a tournament ID 
+  %
   %     user-info - data is a username
   case Action of
     "request_tournament" ->
       case length(TheRest) of
         2 ->
-          NumPlayerString = hd(TheRest),
+          NumPlayersString = hd(TheRest),
           GamePerMatchString = hd(tl(TheRest)),
-          NumPlayer = list_to_integer(NumPlayerString),
+          NumPlayers = list_to_integer(NumPlayersString),
           GamePerMatch = list_to_integer(GamePerMatchString),
-          println("~s > Sending a tournament_request message with data = {~p, ~p}...",
-            [?GlobalName, NumPlayer, GamePerMatch]),
-          {distributed_yahtzee, SystemManagerNode} ! {request_tournament, self(), {NumPlayer, GamePerMatch}};
+          println("~s > Sending a tournament_request message with " ++
+            "data = {~p, ~p}...",
+            [?GlobalName, NumPlayers, GamePerMatch]),
+          {distributed_yahtzee, SystemManagerNode} !
+            {request_tournament, self(), {NumPlayers, GamePerMatch}};
         _ ->
-          halt("Error: There should be exactly two parameters after the keyword 'request_tournament'")
+          halt("Error: There should be exactly two parameters after " ++
+            "the keyword 'request_tournament'")
       end;
     "tournament_info" ->
       case length(TheRest) of
         1 ->
           TournamentId = hd(TheRest),
-           println("~s > Sending a tournament_info message with data = {~p}...",
-          [?GlobalName, TournamentId]),
-          {distributed_yahtzee, SystemManagerNode} ! {tournament_info, self(), {TournamentId}};
+           println("~s > Sending a tournament_info message with " ++
+            "data = {~p}...", [?GlobalName, TournamentId]),
+          {distributed_yahtzee, SystemManagerNode} !
+            {tournament_info, self(), {TournamentId}};
         _ ->
-          halt("Error: There should be exactly one parameter after the keyword 'tournament_info'")
+          halt("Error: There should be exactly one parameter after " ++
+            "the keyword 'tournament_info'")
       end;
     "user_info" ->
       case length(TheRest) of
         1 ->
           Username = hd(TheRest),
-          println("~s > Sending a tournament_request message with data = {~p}...",
-            [?GlobalName, Username]),
-          {distributed_yahtzee, SystemManagerNode} ! {user_info, self(), {Username}};
+          println("~s > Sending a tournament_request message with " ++
+            "data = {~p}...", [?GlobalName, Username]),
+          {distributed_yahtzee, SystemManagerNode} !
+            {user_info, self(), {Username}};
         _ ->
-          halt("Error: There should be exactly one parameter after the keyword 'user_info'")
+          halt("Error: There should be exactly one parameter after " ++
+            "the keyword 'user_info'")
       end;
     _ ->
       halt("Error: Not a valid request.")
@@ -144,10 +173,3 @@ main(Params) ->
 %% ====================================================================
 %%                       Helper Functions
 %% ====================================================================
-% list_to_integer(String) ->
-%   case string:to_integer(String) of
-%      {error, Reason} ->
-%        halt(String ++ " should be a number. " ++ atom_to_list(Reason));
-%      {Int, _} ->
-%       Int
-%   end.
