66df2a63c2c41281f4c6c8e0b1d6d655a14ba934
Vijay Ramakrishnan <vijay120@gmail.com>
2014-04-26 23:12:02 -0700

    pre test referee

--- referee.erl
+++ referee.erl
@@ -1,5 +1,18 @@
+%% CSCI182E - Distributed Systems
+%% Harvey Mudd College
+%% Distributed Yahtzee
+%% @author Tum Chaturapruek, Eoin Nugent, Vijay Ramakrishnan
 -module(referee).
--export([main/1, send_die_from_choice/5]).
+
+-import(distributed_yahtzee, [println/1, println/2]).
+%% ====================================================================
+%%                             Public API
+%% ====================================================================
+-export([referee_main/1]).
+%% ====================================================================
+%%                             Constants
+%% ====================================================================
+-define(GLOBALNAME, "Referee").
 -define(DIESTATE, 6).
 -define(SCORECARDROWS, 13).
 -define(NUMPOSSIBLEDIEOUTCOMES, 15).
@@ -7,7 +20,10 @@
 -define(STARTINDEX, 1).
 -define(FIRSTROUND, 1).
 
-main(Params) ->
+%% ====================================================================
+%%                            Main Function
+%% ====================================================================
+referee_main(Params) ->
 	_ = os:cmd("epmd -daemon"),
 	Reg_name = hd(Params),
 	net_kernel:start([list_to_atom(Reg_name), shortnames]),
@@ -19,15 +35,15 @@ main(Params) ->
 
 findMyPlayersAndGameId() ->
 	receive
-		{Pid, assign_players_and_gameId, PlayerAPid, PlayerBPid, GameId, TournamentId} -> 
+		{Pid, assign_players_and_gameId, PlayerAName, PlayerBName, PlayerAPid, PlayerBPid, GameId, TournamentId} -> 
 			io:format("PlayerAPid is ~p", [PlayerAPid]),
 			io:format("PlayerBPid is ~p", [PlayerBPid]),
 			io:format("GameID is ~p", [GameId]),
-			initiateGame(PlayerAPid, PlayerBPid, GameId, TournamentId);
+			initiateGame(PlayerAName, PlayerAPid, PlayerBName, PlayerBPid, GameId, TournamentId);
 		_ -> io:format("whateves man")
 	end.
 
-initiateGame(PlayerAPid, PlayerBPid, GameId, TournamentId) ->
+initiateGame(PlayerAName, PlayerAPid, PlayerBName, PlayerBPid, GameId, TournamentId) ->
 	random:seed(now()),
 	timer:sleep(100),
 	ScorecardA = generate_fixed_length_lists("scorecard", ?SCORECARDROWS),
@@ -37,25 +53,60 @@ initiateGame(PlayerAPid, PlayerBPid, GameId, TournamentId) ->
 	io:format("A score: ~p", [ScorecardA]),
 	io:format("A die outcomes: ~p", [DieOutcomesA]),
 	io:format("B score: ~p", [ScorecardB]),
-	io:format("B ie outcomes: ~p", [DieOutcomesA]).
+	io:format("B ie outcomes: ~p", [DieOutcomesA]),
 	ChoiceA = [true, true, true, true, true],
 	ChoiceB = [true, true, true, true, true],
-	handle_round(TournamentId, GameId, ?FIRSTROUND, ScorecardA, ScorecardB, DieOutcomesA, DieOutcomesB, PlayerAPid, PlayerBPid, ChoiceA, ChoiceB).
+	handle_round(	TournamentId, 
+					GameId, 
+					?FIRSTROUND, 
+					PlayerAName, PlayerBName, 
+					ScorecardA, ScorecardB, 
+					DieOutcomesA, DieOutcomesB, 
+					PlayerAPid, PlayerBPid, 
+					ChoiceA, ChoiceB
+				).
 
 
- handle_round(Tid, Gid, Round, PlayerAScore, PlayerBScore, DieOutcomesA, DieOutcomesB, PlayerAPid, PlayerBPid, ChoiceA, ChoiceB) ->
+handle_round(	Tid, 
+				Gid, 
+				Round, 
+				PlayerAName, PlayerBName, 
+				PlayerAScore, PlayerBScore, 
+				DieOutcomesA, DieOutcomesB, 
+				PlayerAPid, PlayerBPid, 
+				ChoiceA, ChoiceB) ->
 
- 	%Step 1: Calculate the dies that need to be send for each player
- 	DieToA = send_die_from_choice(DieOutcomesA, ChoiceA, Round, ?STARTINDEX, []),
- 	%Step 2: Send the message!
- 	PlayerAPid ! {make_ref(), Tid, Gid, Round, DieToA, PlayerAScore, PlayerBScore},
+	if Round > 3 ->
+		%The last round is over, so pass the results to the tournament manager
+		io:format("I will have to do this later");
+		true -> 
+			%Step 1: Calculate the dies that need to be send for each player
+			DieToA = send_die_from_choice(DieOutcomesA, ChoiceA, Round, ?STARTINDEX, []),
+			%Step 2: Send the message!
+			{player, vijay120@ash} ! {play_request, self(), PlayerAName, {make_ref(), Tid, Gid, Round, DieToA, PlayerAScore, PlayerBScore}},
 
- 	DieToB = send_die_from_choice(DieOutcomesB, ChoiceB, Round, ?STARTINDEX, []),
- 	PlayerAPid ! {make_ref(), Tid, Gid, Round, DieToB, PlayerBScore, PlayerAScore},
+			DieToB = send_die_from_choice(DieOutcomesB, ChoiceB, Round, ?STARTINDEX, []),
+			{player, vijay120@lothlorien}  ! {play_request, self(), PlayerBName, {make_ref(), Tid, Gid, Round, DieToB, PlayerBScore, PlayerAScore}},
 
- 	receive
- 		{Ref, Tid, Gid, roll-number, dice-to-keep, scorecard-line }
-
+			%Recieve for player A only
+			receive
+				{play_action, PidA, PlayerAName, {RefA, TidA, GidA, RollNumberA, DiceToKeepA, ScorecardA}} -> 
+					%Receive for player B only
+					receive
+						{play_action, PidB, PlayerBName, {RefB, TidB, GidB, RollNumberB, DiceToKeepB, ScorecardB}} -> 
+							handle_round(	Tid, 
+											Gid, 
+											Round+1, 
+											PlayerAName, PlayerBName, 
+											ScorecardA, ScorecardB, 
+											DiceToKeepA, DiceToKeepB, 
+											PidA, PidB, 
+											DiceToKeepA, DiceToKeepB);
+						_ -> io:format("Invalid message type")
+					end;
+				_ -> io:format("Invalid message type")
+			end
+		end.
 
 
 
@@ -73,7 +124,6 @@ send_die_from_choice(DieSequence, Choice, Round, CurrentIndex, AccumulatedDieSeq
 		end
 	end.
 
-
 generate_fixed_length_lists(Type, Count) ->
 	timer:sleep(10),
 	if Type == "scorecard" ->
@@ -86,72 +136,8 @@ generate_fixed_length_lists(Type, Count) ->
 		end
 	end.
 
-=======
-%% CSCI182E - Distributed Systems
-%% Harvey Mudd College
-%% Distributed Yahtzee
-%% @author Tum Chaturapruek, Eoin Nugent, Vijay Ramakrishnan
--module(referee).
-
--import(distributed_yahtzee, [println/1, println/2]).
-%% ====================================================================
-%%                             Public API
-%% ====================================================================
--export([referee_main/1]).
-%% ====================================================================
-%%                             Constants
-%% ====================================================================
--define(GLOBALNAME, "Referee").
--define(DIESTATE, 6).
--define(SCORECARDROWS, 14).
--define(NUMPOSSIBLEDIEOUTCOMES, 15).
-
-%% ====================================================================
-%%                            Main Function
-%% ====================================================================
-referee_main(Params) ->
-  os:cmd("epmd -daemon"),
-  Reg_name = hd(Params),
-  net_kernel:start([list_to_atom(Reg_name), shortnames]),
-  printnameln("My node is ~p", [node()]),
-  printnameln("My pid is ~p", [self()]),
-  register(referee, self()),
-  findMyPlayersAndGameId().
-
 getName() ->
   ?GLOBALNAME ++ io_lib:format("~p", [self()]).
-
-findMyPlayersAndGameId() ->
-  receive
-    {Pid, assign_players_and_gameId, PlayerAPid, PlayerBPid, GameId, TournamentId} -> 
-      printnameln("PlayerAPid is ~p", [getName() ,PlayerAPid]),
-      printnameln("PlayerBPid is ~p", [PlayerBPid]),
-      printnameln("GameID is ~p", [GameId]),
-      initiateGame(PlayerAPid, PlayerBPid, GameId, TournamentId);
-    _ -> printnameln("whateves man", [getName()])
-  end.
-
-initiateGame(PlayerAPid, PlayerBPid, GameId, TournamentId) ->
-  random:seed(now()),
-  timer:sleep(100),
-  ScorecardA = generate_fixed_length_lists("scorecard", ?SCORECARDROWS),
-  DieOutcomesA = generate_fixed_length_lists("random", ?NUMPOSSIBLEDIEOUTCOMES),
-  ScorecardB = generate_fixed_length_lists("scorecard", ?SCORECARDROWS),
-  DieOutcomesB = generate_fixed_length_lists("random", ?NUMPOSSIBLEDIEOUTCOMES),
-  printnameln("A score: ~p", [ScorecardA]),
-  printnameln("A die outcomes: ~p", [DieOutcomesA]),
-  printnameln("B score: ~p", [ScorecardB]),
-  printnameln("B ie outcomes: ~p", [DieOutcomesA]).
-
-
-generate_fixed_length_lists(Type, Count) ->
-  timer:sleep(100),
-  if Count == 0 -> [];
-    true ->   
-      if Type == "scorecard" -> [-1] ++ generate_fixed_length_lists(Type, Count-1);
-        true -> [element(1, random:uniform_s(?DIESTATE, random:seed(now())))] ++ generate_fixed_length_lists(Type, Count-1)
-    end
-  end.
 
 printnameln(ToPrint) ->
   println(io_lib:format("~s > ", [getName()]) ++ ToPrint).
